bdaf9f209686a95e4953e15d65755a90f7cb8682
Baptiste Lemaire <b.lemaire@kiabi.com>
2017-10-20 14:29:11 +0200

    Suite

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
     imports: [ BrowserModule,
-        FormsModule],
+        FormsModule,
+        RouterModule.forRoot([
+          {
+            path: 'heores',
+            component : HeroesComponent
+          }
+        ])],
     declarations: [
       AppComponent,
+      HeroesComponent,
       HeroDetailComponent ],
+    providers:[HeroService],
     bootstrap: [ AppComponent ]
 })
+
+
 export class AppModule { }
